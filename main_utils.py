0faa58ba2a1f0f3d7ef8243c2e5c3634f03cc494
Hobospider132 <100825450+Hobospider132@users.noreply.github.com>
2023-03-01 18:04:57 +1100

    Update main_utils.py

--- main_utils.py
+++ main_utils.py
@@ -1,29 +1,17 @@
 """The main cog. Contains 'mainstream' utilities."""
 import asyncio
 import logging
-import re
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-from variables import VERSION
-
 class Utility(commands.Cog, name="Main Utilities"):
     """Main bot utilities"""
 
- def __init__(self, model_name):
-        # adding intents module to prevent intents error in __init__ method in newer versions of Discord.py
-        intents = discord.Intents.default() # Select all the intents in your bot settings as it's easier
-        intents.message_content = True
-        super().__init__(intents=intents)
-        self.api_endpoint = API_URL + model_name
-        # retrieve the secret API token from the system environment
-        huggingface_token = os.environ['HUGGINGFACE_TOKEN']
-        # format the header in our request to Hugging Face
-        self.request_headers = {
-            'Authorization': 'Bearer {}'.format(huggingface_token)
-        }
+    def __init__(self, bot: commands.Bot, config: Config):
+        self._config = config
+        self._bt = bot
         logging.info("Loaded %s", self.__class__.__name__)
 
     @app_commands.command(
@@ -36,10 +24,6 @@ class Utility(commands.Cog, name="Main Utilities"):
             + str(round(self._bt.latency * 1000))
             + "ms"
         )
-        await self._bt.change_presence(
-            activity=discord.Activity(
-                type=discord.ActivityType.playing, name="with tickets!"
-            )
-        )
-
-
+			
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Utility(bot))
