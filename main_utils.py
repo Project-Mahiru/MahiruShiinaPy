fe697942b7bfdfd33f639385de34b70a9634c995
Hobospider132 <100825450+Hobospider132@users.noreply.github.com>
2023-03-01 20:46:26 +1100

    Add code for API request

--- main_utils.py
+++ main_utils.py
@@ -4,6 +4,21 @@ import logging
 from discord import app_commands
 from discord.ext import commands
 
+import json
+import requests
+
+def query(self, payload):
+		"""
+		make request to the Hugging Face model API
+		"""
+		data = json.dumps(payload)
+		response = requests.request('POST',
+																'https://api-inference.huggingface.co/models/Hobospider132/DialoGPT-Mahiru-Proto',
+																headers={ 'Authorization': 'Bearer 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' },
+																data=data)
+		ret = json.loads(response.content.decode('utf-8'))
+		return ret
+
 class Utility(commands.Cog, name="Main Utilities"):
     """Main bot utilities"""
 
@@ -15,12 +30,18 @@ class Utility(commands.Cog, name="Main Utilities"):
         name="ping", description="The classic ping command. Checks the bot's latency."
     )
     async def ping(self, ctx):
-        """This command is used to check if the bot is online."""
-        await ctx.response.send_message(
-            "Pong! The bot is online.\nPing: "
-            + str(round(self._bt.latency * 1000))
-            + "ms"
-        )
+        payload = {'inputs': {'text': ctx.content}}
+        response = self.query(payload)
+        bot_response = response.get('generated_text', None)
+
+        if not bot_response:
+            if 'error' in response:
+                bot_response = '`Error: {}`'.format(response['error'])
+            else:
+                bot_response = 'Hmm... something is not right.'
+
+        # send the model's response to the Discord channel
+        await ctx.channel.send(bot_response)
 
 async def setup(bot: commands.Bot):
     """Setup function for the cog."""
